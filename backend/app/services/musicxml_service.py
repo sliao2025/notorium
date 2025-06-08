6502978cd0826fb454fd632906294756455f4247
Steven Liao <steven_y_liao@brown.edu>
2025-06-08 16:56:47 -0400

    fixed routers

--- backend/app/services/musicxml_service.py
+++ backend/app/services/musicxml_service.py
@@ -2,7 +2,12 @@ import shutil
 import uuid
 from pathlib import Path
 from fastapi import UploadFile
-from music21 import converter
+from music21 import converter,midi
 from app.core.config import settings
 from app.schemas.musicxml import UploadResponse
 
+async def process_musicxml(file):
+    midi_url = None
+    xml_url = None
+    midifile = midi.translate.streamToMidiFile(converter.parse(file))
+    return(UploadResponse(xml_url,midi_url))
